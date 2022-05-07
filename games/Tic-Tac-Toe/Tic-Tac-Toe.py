f4a587ecfa26958dfdbd87aefba4d4cb0d6ba836
HO-Vic <shp9826@naver.com>
2022-05-08 08:42:23 +0900

    이미지 나오게 성공/ o,x,empty 포토 글로벌화/ window = Tk()객체 글로벌화

--- games/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ games/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -7,23 +7,26 @@ image_empty_path = "empty.gif"
 
 class Cell(Label): # 하나의 그림 1 부분
     cellStatus = 0
-    def __init__(self, window):
-        photoInit = PhotoImage(file = "empty.gif")
-        
-        #PhotoImage(file = image_o_path)
-        super().__init__(window, image = photoInit, width=23, height=13)
-        super().pack()
-  
+    def __init__(self):
+        global window
+        super().__init__(window, width=200, height=100)
+        global photo_Empty        
+        self.configure(image = photo_Empty)
+       
 
+window = Tk()
+photo_Empty = PhotoImage(file = image_empty_path)
+photo_O = PhotoImage(file = image_o_path)
+photo_X = PhotoImage(file = image_x_path)
 
 def main():
-    window = Tk()
+    global window
+    
     window.title('Tic-Tac-Toe')
     window.geometry("600x600+450+100")
     window.resizable(False, False)
 
-    c1 = Cell(window)
-    c1.configure(image = image_o_path) 
+    c1 = Cell()
     c1.pack(side = 'top', anchor= 'w')
 
 
