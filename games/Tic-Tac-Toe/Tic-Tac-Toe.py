27c2638389eacc9cb2b0481fc30735bec1261470
HO-Vic <shp9826@naver.com>
2022-05-08 18:38:27 +0900

    눌렀을때, 사진 바뀌게 하는것 구현, 다시 눌렀을때 empty가 아니라면 사진 바뀌게 하는거 아직 미구현

--- games/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ games/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -5,29 +5,52 @@ image_x_path = "x.gif"
 image_empty_path = "empty.gif"
 
 
-class Cell(Label): # 하나의 그림 1 부분
-    cellStatus = 0
-    def __init__(self):
-        global window
-        super().__init__(window, width=200, height=100)
-        global photo_Empty        
-        self.configure(image = photo_Empty)
-       
-
 window = Tk()
 photo_Empty = PhotoImage(file = image_empty_path)
 photo_O = PhotoImage(file = image_o_path)
 photo_X = PhotoImage(file = image_x_path)
 
+gameTurn = 0
+
+class Cell(Label):
+    cellStatus = 0
+    def __init__(self, row = 0, column = 0):
+        global window
+
+        global photo_Empty
+
+        super().__init__(window, width=34, height=34)
+        super().grid(row = row, column = column)
+        super().configure(image = photo_Empty)
+        super().bind("<Button-1>", lambda e: self.changePhoto())
+        
+    
+    def changePhoto(event):
+        global gameTurn
+        if gameTurn == 0:
+            super().configure(image = photo_O)
+            gameTurn = 1
+        else:
+            super().configure(image = photo_X)
+            gameTurn = 0
+    
+    def setCellStatus(s):
+        cellStatus = s
+
+    
+
+
 def main():
     global window
-    
+   
     window.title('Tic-Tac-Toe')
-    window.geometry("600x600+450+100")
+    window.geometry("115x125+450+100")
     window.resizable(False, False)
 
-    c1 = Cell()
-    c1.pack(side = 'top', anchor= 'w')
+    board = [
+        [Cell(row = 0, column = 0), Cell(row = 0, column = 1), Cell(row = 0, column = 2)],
+     [Cell(row = 1, column = 0), Cell(row = 1, column = 1), Cell(row = 1, column = 2)],
+     [Cell(row = 2, column = 0), Cell(row = 2, column = 1), Cell(row = 2, column = 2)]]
 
 
     window.mainloop()
