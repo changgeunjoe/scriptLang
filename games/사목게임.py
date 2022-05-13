d01347895504035d0b9a3e00cf1d42acaf8c4b5c
changgeunjoe <hoi000115@gmail.com>
2022-05-13 19:31:52 +0900

    main추가 화면 띄우기추가,클릭추가 색깔 추가

--- games/사목게임.py
+++ games/사목게임.py
@@ -4,8 +4,38 @@ from tkinter import *
 MaxRow = 6
 MaxCol = 7
 
+from tkinter import *
+window = Tk()
+_MAXROW = 6
+_MAXCOL = 7
+process_button = None#하단의 버튼
+myText = Label(window, text = "새로 시작", anchor = 'center', width=0, height= 1, fg = 'red', bg = 'blue')
 class Cell(Canvas):
-    pass
+    cellStatus = 0
+    def __init__(self, row = 0, column = 0,cellnums=0):
+        self.color = "white"
+        cellnum = cellnums
+        cellStatus = 0
+        global window
+        super().__init__(window, width=20, height=20,bg="blue")
+        super().grid(row = row, column = column)
+        #global photo_Empty
+        #super().configure(image = photo_Empty)
+        super().create_oval(4, 4, 20, 20, fill = "white", tags="oval")
+        super().bind("<Button-1>", self.clicked)
+       # super().bind("<Button-1>", lambda e: self.changePhoto(cellnum = cellnum, obj = self))
+    def clicked(self, event): # red 또는 yellow 돌 놓기.
+        nextcolor = "red" 
+        if self.color != "yellow":
+            nextcolor="red" 
+        else: 
+            nextcolor="red"
+        self.setColor(nextcolor)
+    def setColor(self, color):
+        self.delete("oval") # https://pythonguides.com/python-tkinter-canvas/
+        self.color = color
+        self.create_oval(4, 4, 20, 20, fill = self.color, tags="oval")
+
 
 
 def continueColStone(cells):
@@ -55,3 +85,23 @@ def continueRowStone(cells):
 def continuedialogRDStone(cells):
     while(True):
         pass
+
+def main():
+    global window
+   
+    window.title('Tic-Tac-Toe')
+    window.geometry("133x145+450+100")
+
+    global board
+    board = [
+    [Cell(row = 0, column = 0, cellnums=0), Cell(row = 0, column = 1,cellnums=1), Cell(row = 0, column = 2,cellnums=2),Cell(row = 0, column = 3,cellnums=3),Cell(row = 0, column = 4,cellnums=4),Cell(row = 0, column = 5,cellnums=5),Cell(row = 0, column = 6,cellnums=6)],
+    [Cell(row = 1, column = 0, cellnums=0), Cell(row = 1, column = 1,cellnums=1), Cell(row = 1, column = 2,cellnums=2),Cell(row = 1, column = 3,cellnums=3),Cell(row = 1, column = 4,cellnums=4),Cell(row = 1, column = 5,cellnums=5),Cell(row = 1, column = 6,cellnums=6)],
+    [Cell(row = 2, column = 0, cellnums=0), Cell(row = 2, column = 1,cellnums=1), Cell(row = 2, column = 2,cellnums=2),Cell(row = 2, column = 3,cellnums=3),Cell(row = 2, column = 4,cellnums=4),Cell(row = 2, column = 5,cellnums=5),Cell(row = 2, column = 6,cellnums=6)],
+    [Cell(row = 3, column = 0, cellnums=0), Cell(row = 3, column = 1,cellnums=1), Cell(row = 3, column = 2,cellnums=2),Cell(row = 3, column = 3,cellnums=3),Cell(row = 3, column = 4,cellnums=4),Cell(row = 3, column = 5,cellnums=5),Cell(row = 3, column = 6,cellnums=6)],
+    [Cell(row = 4, column = 0, cellnums=0), Cell(row = 4, column = 1,cellnums=1), Cell(row = 4, column = 2,cellnums=2),Cell(row = 4, column = 3,cellnums=3),Cell(row = 4, column = 4,cellnums=4),Cell(row = 4, column = 5,cellnums=5),Cell(row = 4, column = 6,cellnums=6)],
+    [Cell(row = 5, column = 0, cellnums=0), Cell(row = 5, column = 1,cellnums=1), Cell(row = 5, column = 2,cellnums=2),Cell(row = 5, column = 3,cellnums=3),Cell(row = 5, column = 4,cellnums=4),Cell(row = 5, column = 5,cellnums=5),Cell(row = 5, column = 6,cellnums=6)]]
+    myText.grid(row=6, column = 1, padx = 0, sticky = 'n')
+    
+    window.mainloop()
+
+main()
