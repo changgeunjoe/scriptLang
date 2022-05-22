e983f3480926cb14130f82625d4113f150925889
HO-Vic <shp9826@naver.com>
2022-05-23 05:49:59 +0900

    hangman 그려지는 카운트 수정, 정답일때, 게임이 끝났을때, enter를 이용해서 재시작하게 함

--- games/hangMan/hangMan.py
+++ games/hangMan/hangMan.py
@@ -5,12 +5,11 @@ words = []
 secretWord = '****'
 answerWord = ''
 wrongWords = []
-wrongCount=7
+wrongCount=6
 window = Tk()
 
 
-def initWords():#단어 리스트 파일 읽어 오는 부분
-    global words
+def initWords():
     hangmanFile = open('txt\hangman.txt', 'r')    
     while(True):
         word = hangmanFile.read()
@@ -20,7 +19,7 @@ def initWords():#단어 리스트 파일 읽어 오는 부분
             words.append(w)
     hangmanFile.close()
 
-def getRandWord():#answerWord = getRandWord()
+def getRandWord():
     global words
     global answerWord
     randWN = randI(0, len(words))
@@ -33,7 +32,7 @@ def getRandWord():#answerWord = getRandWord()
     return answerWord
 
 
-def isContainWord(inputWord):#현재 입력한 문자를 답인 문자에 포함 되어 있는지 판단 -> 존재 True /  False -> 행맨 부위 자르기// 반환은 bool, str(입력 문자)
+def isContainWord(inputWord):
     isFind = False
     global answerWord
     global secretWord
@@ -50,28 +49,39 @@ def changeWord():
             return False
     return True
 
-def clicked(event, obj):        
-        print('clicked')
-
 def keyEvent(event):
     hang.delete('keyevent')
     hang.delete('secretword')
     hang.delete('hangman')
-    hang.create_text(280,210,text = (repr(event.char))[1], tags='keyevent')
-    global wrongWords
-    res = isContainWord(repr(event.char)[1])
-    if not res[0]:
-        global wrongCount
-        if wrongWords.count(res[1]) != 1:
-            wrongCount-=1
-            wrongWords.append(res[1])
-            hang.delete('wrongevent')
-            hang.create_text(280,240,text = wrongWords, tags='wrongevent')
-    hang.draw()
-    #96번줄 secretWord 다시 출력 되게 해야 됨
+    global wrongCount
+    if wrongCount != -1:
+        hang.create_text(280,210,text = (repr(event.char))[1], tags='keyevent')
+        global wrongWords
+        res = isContainWord(repr(event.char)[1])
+        if not res[0]:
+            if wrongWords.count(res[1]) != 1:
+                wrongCount-=1
+                wrongWords.append(res[1])
+                hang.delete('wrongevent')
+                hang.create_text(280,240,text = wrongWords, tags='wrongevent')
+        hang.draw()
 
-def inputReturn(event, obj):        
-    print('enter')
+def inputReturn(event):     
+    global wrongCount
+    global wrongWords
+    global hang
+    if (not changeWord() and wrongCount == -1) or changeWord():
+        wrongCount = 6
+        getRandWord()
+        wrongWords.clear()
+        hang.draw()
+        hang.delete('keyevent')
+        hang.delete('secretword')
+        hang.delete('hangman')
+        hang.delete('wrongevent')
+        hang.delete('regame')
+        hang.create_text(280,190,text=secretWord,tags='secretword')
+        
 
 def inputBackSpace(event, obj):
     print('backSpace')
@@ -88,11 +98,7 @@ class Hangman(Canvas):
         super().grid(row=0, column=0)
         self.draw()
         window.bind("<Key>", keyEvent)
-        #super().bind("<Button-1>", lambda e: self.clicked(obj=self))
-        # super().bind("<Return>", lambda e: self.inputReturn(obj=self))
-        # super().bind("<BackSpace>", lambda e: self.inputBackSpace(obj=self))
-    
-    
+        window.bind("<Return>", inputReturn)
 
    
     def guess(self,letter):
@@ -107,19 +113,6 @@ class Hangman(Canvas):
         self.create_arc(20,200,20+80,200+40,start=0,extent=180)
         self.create_line(20+40,200,20+40,20)
         self.create_line(20+40,20,20+140,20)
-    
-        # if self.doneWithWrong:
-        #     self.canvas.create_text(200,250,text='정답'+self.hiddenword,tags='hangman')
-        #     self.canvas.create_text(200,265,text='계속하면 Enter',tags='hangman')
-        # if self.doneWithCorrect:
-        #     self.canvas.create_text(200,250,text='맞았습니다'+self.hiddenword,tags='hangman')
-        #     self.canvas.create_text(200,265,text='계속하면 Enter',tags='hangman')
-        # else:
-        #     self.canvas.create_text(200,250,text='단어 추측'+self.toString(self.guessWord),tags='hangman')
-        #     if self.NofMiss>0:
-        #         self.canvas.create_text(200,260,text='단어 추측'+self.toString(self.missChars),tags='hangman')
-        # if self.NofMiss<1:
-        #     return
         x1=20+140
         y1=20
         x2=x1
@@ -127,20 +120,30 @@ class Hangman(Canvas):
         self.create_line(x1,y1,x2,y2,tags='hangman')
         x3=x2
         y3=y2+20
-        if wrongCount < 7:
+        if wrongCount < 6:
             self.create_oval(x3-20,y3-20,x3+20,y3+20,tags='hangman')
-        if wrongCount < 6:    
-            self.create_line(x3-15,y3+15,x3-50,y3+70,tags='hangman')
         if wrongCount < 5:    
+            self.create_line(x3-15,y3+15,x3-50,y3+70,tags='hangman')
+        if wrongCount < 4:    
             self.create_line(x3+15,y3+15,x3+50,y3+70,tags='hangman')
         x4=x3
         y4=y3+100
-        if wrongCount < 4:
-            self.create_line(x3,y3+20,x4,y4,tags='hangman')
         if wrongCount < 3:
+            self.create_line(x3,y3+20,x4,y4,tags='hangman')
+        if wrongCount < 2:
             self.create_line(x4,y4,x4-50,y4+100,tags='hangman')
-        if wrongCount < 2:    
+        if wrongCount < 1:    
             self.create_line(x4,y4,x4+50,y4+100,tags='hangman')
+
+        if changeWord():
+            global answerWord
+            self.create_text(200,260,text='정답입니다. 다시 시작하려면 enter를 입력하세요', tags = 'regame')
+
+
+        if not changeWord() and wrongCount == -1:
+            global answerWord
+            self.create_text(200,260,text='정답은' + answerWord + '다시 시작하려면 enter를 입력하세요', tags = 'regame')
+
     
 hang = 0
 
