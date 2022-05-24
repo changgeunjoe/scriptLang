d030e32bfd3168c6e6e46591941949795999dc0c
HO-Vic <shp9826@naver.com>
2022-05-24 10:33:08 +0900

    기본 클래스 생성

---
+++ apt/aptP.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class aptApp(Canvas):
+    selectRegion = Button()
+    searchButton = Button()
+    myList = Button()
+    atpInformation = Button()
+    aptPhoto = Canvas()
+    #email
+    #graph =  #수업시간에 할거
+    #aptMap = 
+
