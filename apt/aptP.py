e083436b225766713fe6a4e2602d5df27e25baaf
HO-Vic <shp9826@naver.com>
2022-05-24 10:48:08 +0900

    매우 매우 매우 기본적인 class만 제작함 나머지 gui창근이가 해야됨

--- apt/aptP.py
+++ apt/aptP.py
@@ -1,12 +1,32 @@
+from msilib.schema import ListBox
 from tkinter import *
+from turtle import width
+
+window = Tk()
+
+class regionButton(Button):
+    def __init__(self):
+        super().__init(text = "지역 선택", anchor = 'center', width = 40, height = 20)
+        
+        
+
 
 class aptApp(Canvas):
-    selectRegion = Button()
-    searchButton = Button()
-    myList = Button()
+    logo = Label()
+    selectRegion = regionButton()
+    searchButton = Entry()
+    myList = Listbox()
     atpInformation = Button()
     aptPhoto = Canvas()
     #email
     #graph =  #수업시간에 할거
     #aptMap = 
+
+    def __init__(self):
+        self.color = "white"                
+        global window
+        super().__init__(window, width=400, height=300, bg="white")
+        super().grid(row=0, column=0)
+        self.draw()
+        
 
